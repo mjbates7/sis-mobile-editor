50d04c40a785edd55dd64650595e045bc93dd3cb
Martin Bates <mbates@sis.tv>
2017-02-20 17:39:47 +0000

    Updated meetings json

--- src/containers/meeting/index.js
+++ src/containers/meeting/index.js
@@ -8,10 +8,29 @@ class MeetingPage extends Component {
         this.state = {
             meetings: []
         }
+
+        this.buildCard = this.buildCard.bind(this);
     }
 
     componentDidMount() {
-        this.setState({ meetings: [{"id":"1","meetingDate":"17-02-2017","venue":"Crayford","category":"DG","status":"On","country":"UK","authority":"BA","going":"Good to firm","serviceType":"Full","numerOfRaces":11,"firstEventTime":"14:08","lastEventTime":"18:11","eventTimes":["14:08","14:27","14:47","15:07","15:28","15:48","16:08","16:27","16:47","17:07","17:22","17:38","17:56","18:11"]}]});
+        this.setState({ 
+            meetings: [{"id":"1","meetingDate":"17-02-2017","venue":"Peterborough","category":"DG","status":"On","country":"UK","authority":"BA","going":"Good to firm","serviceType":"Full","numerOfRaces":11,"event":{"progress":{"name":"Going In Traps","code":"E"},"firstEventTime":"14:08","lastEventTime":"18:11","eventTimes":["14:08","14:27","14:47","15:07","15:28","15:48","16:08","16:27","16:47","17:07","17:22","17:38","17:56","18:11"]}}]
+        });
+    }
+
+    buildCard(meeting) {
+        return (
+            <div key={meeting.id} className="uk-card uk-card-default uk-card-small uk-card-body">
+                <div className="uk-card-badge uk-label">{meeting.event.progress.name}</div>
+                <div className="uk-card-title">{meeting.venue}</div>
+                <p>
+                    body
+                </p>
+                <div className="uk-card-footer">
+                    
+                </div>
+            </div>
+        );
     }
 
     render() {
@@ -21,12 +40,7 @@ class MeetingPage extends Component {
         return (
             <div>
                 <p>found : { abc } meetings</p>
-                <div className="uk-card uk-card-default uk-card-small uk-card-body">
-                    <div className="uk-card-title">title</div>
-                </div>
-                <div className="uk-card uk-card-default uk-card-small uk-card-body">
-                    <div className="uk-card-title">title</div>
-                </div>
+                {this.state.meetings.map(meeting => this.buildCard(meeting))}
             </div>
         );
     }
@@ -35,20 +49,5 @@ class MeetingPage extends Component {
 export default MeetingPage;
 
 /*
-<table className="uk-table">
-                <caption>Dog Meetings - {this.state.meetings.length}</caption>
-                <thead>
-                    <tr>
-                        <th>Venue</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.meetings.map(meeting => {
-                    return (
-                        <tr>
-                            <td>{meeting.venue}</td>
-                        </tr>)
-                    })}
-                </tbody>
-            </table>*/
+{meeting.event.eventTimes.map(eventTime => <span key={eventTime} className="uk-label uk-margin-small-right">{eventTime}</span>)}
+*/
